11a74cefc2491cb5a47dc4e00fbd786c0b7abfc6
evelina-meijere <124715450+evelina-meijere@users.noreply.github.com>
2023-02-16 10:36:37 +0000

    updated 16.02.

--- main.py
+++ main.py
@@ -11,21 +11,51 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
+    for i, next in enumerate(text): # enumarete funkcija dod atpakaļ pašreizējo iterāciju skaitu un vērtību
+        if next in "([{": 
+            opening_brackets_stack.append(Bracket(next, i )) # append funkcija pievieno elementu saraksta beigās
             # Process opening bracket, write your code here
-            pass
+           
 
         if next in ")]}":
+            if not opening_brackets_stack: return i+1 
+            m = opening_brackets_stack.pop() 
+            if not are_matching: 
+                return i+1 # pop funkcijas izņemt specifiskā pozīcījā esošu elementu
+           
+
+            if opening_brackets_stack:
+                m = opening_brackets_stack.pop() 
+                return m.position + 1 
+            return ("Success")
             # Process closing bracket, write your code here
-            pass
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print("1. Nolasa no faila, 2. Ievada pats: Izvēlies 1 vai 2")
+    atbilde = input()
+    if "1" in atbilde:
+     failanos = input ("Faila nosaukums: ")
+     with open(failanos, "r") as file:
+        iekavas = file.read()
+        mekle = find_mismatch(iekavas)
+        if mekle == "Success":
+            print("Success")
+        else:
+            print(mekle)
+    elif "2" in atbilde:
+        iekavas = input()
+        mekle = find_mismatch(iekavas)
+        if mekle == "Success":
+            print("Success")
+        else:
+            print(mekle)
+    else:
+        print("Ievadīts nepareizs cipars!")
 
+    # Printing answer, write your code here
+ 
 
 if __name__ == "__main__":
     main()
+
